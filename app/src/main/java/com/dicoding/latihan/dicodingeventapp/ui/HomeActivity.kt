39bdd083654985c747b3bfbce9c55da695d76c7b
Mohammad Bulghotul Khowash <mohammadbulghotulkhowashh@gmail.com>
2025-02-07 20:29:26 +0700

    feat: add setting light and dark mode

--- app/src/main/java/com/dicoding/latihan/dicodingeventapp/ui/HomeActivity.kt
+++ app/src/main/java/com/dicoding/latihan/dicodingeventapp/ui/HomeActivity.kt
@@ -3,20 +3,34 @@ package com.dicoding.latihan.dicodingeventapp.ui
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.dicoding.latihan.dicodingeventapp.R
+import com.dicoding.latihan.dicodingeventapp.data.datastore.DataStoreManager
+import com.dicoding.latihan.dicodingeventapp.data.datastore.dataStore
 import com.dicoding.latihan.dicodingeventapp.databinding.ActivityHomeBinding
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        val dataStore = DataStoreManager.getInstance(applicationContext.dataStore)
 
+        runBlocking {
+            val isDarkMode = dataStore.getThemeSetting().first()
+            AppCompatDelegate.setDefaultNightMode(
+                if (isDarkMode == true) AppCompatDelegate.MODE_NIGHT_YES
+                else AppCompatDelegate.MODE_NIGHT_NO
+            )
+        }
+
+        super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -27,7 +41,7 @@ class HomeActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_event_upcoming, R.id.navigation_event_finished
+                R.id.navigation_home, R.id.navigation_event_upcoming, R.id.navigation_event_finished, R.id.navigation_settings
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
